987f40839e1974a27af1d3d4a49ec186bf26b607
Anurag Shukla <anurag2125it@gmail.com>
2026-08-05 15:55:57 +0530

    Created Pages and Routes

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthPage = () => {
+  return (
+    <div>
+      <div>AuthPage</div>
+    </div>
+  );
+};
+
+export default AuthPage;
---
+++ client/src/pages/PreviewPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PreviewPage = () => {
+  return (
+    <div>
+      <div>PreviewPage</div>
+    </div>
+  );
+};
+
+export default PreviewPage;
